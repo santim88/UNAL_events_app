d2f93a99854709a2fa0729571dab8faeb304b078
GGomezMorales <gugomog3@gmail.com>
2024-02-02 20:13:39 -0500

    feat: implemented an animation for splash screen

---
+++ app/src/main/java/com/unalminas/eventsapp/presentation/screens/splash/SplashScrenn.kt
@@ -0,0 +1,48 @@
+package com.unalminas.eventsapp.presentation.screens.splash
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.unalminas.eventsapp.R
+import com.unalminas.eventsapp.presentation.Screen
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(Unit) {
+        scale.animateTo(
+            targetValue = 0.5f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(1000L)
+        navController.navigate(Screen.HomeScreen.EventsRoute.route)
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier.scale(scale.value),
+            painter = painterResource(id = R.drawable.mock_image),
+            contentDescription = "Icon")
+    }
+}
